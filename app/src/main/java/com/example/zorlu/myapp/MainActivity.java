c89d305be86c3116e0160e9e07ef028b5e280323
amerty95 <amerty95@hotmail.com>
2016-07-11 09:32:18 +0300

    day 11

--- app/src/main/java/com/example/zorlu/myapp/MainActivity.java
+++ app/src/main/java/com/example/zorlu/myapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.zorlu.myapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.zorlu.mylibrary.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println(Test.getString());
     }
 }
